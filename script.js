e83bfe9094c7e023950282a594e4e413f2990690
ClementMonard <clement.monard@hotmail.fr>
2018-06-25 13:49:39 +0200

    Correction mineur

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+//Déclaration de la fonction qui permet de vider les champs du formulaire.
 function deletingText() {
-  document.getElementById('lastname').value = ' '
-  document.getElementById('firstname').value = ' '
-  document.getElementById('city').value = ' '
+  document.getElementById('lastname').value = ' ';
+  document.getElementById('firstname').value = ' ';
+  document.getElementById('city').value = ' ';
 }
